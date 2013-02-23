bda2d9be65245a0092dbb23dc1ac202b3c801f1c
Albert Peng <pair+albert.peng@socialchorus.com>
2013-02-23 01:15:47 -0800

    Adds LnklyGem

--- lib/lnkly_gem.rb
+++ lib/lnkly_gem.rb
@@ -1,2 +1,14 @@
-require "httparty"
+#Requires httparty but will eventually use just Net::HTTP
+require 'httparty'
 
+class Lnkly
+  USERNAME = ENV['LNKLY_USERNAME']
+  PASSWORD = ENV['LNKLY_PASSWORD']
+  BASE_URL = "http://localhost:5000"
+
+  def self.shorten(url)
+    composite_url = BASE_URL + "/create"
+    response = HTTParty.post(composite_url, body: {url: url}, basic_auth: {username: USERNAME, password: PASSWORD})
+    response.parsed_response
+  end
+end
--- spec/lnkly_gem_spec.rb
+++ spec/lnkly_gem_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
-describe "Foo" do
-  it "should be success" do
-    true.should be_true
+ENV['LNKLY_USERNAME'] = "username"
+ENV['LNKLY_PASSWORD'] = "password"
+
+require 'lnkly_gem'
+
+describe Lnkly do
+  it "should return a shortened link" do
+    shortened = Lnkly.shorten("foo")
+    shortened.should_not == "foo"
+    shortened.should be_a String
+  end
+
+  it "should redirect to the correct location" do
+    shortened = Lnkly.shorten("http://www.example.com")
+    response = HTTParty.get(shortened, follow_redirects: false)
+    [301, 302].should include(response.code)
+    response.headers["location"].should == "http://www.example.com"
   end
 end
